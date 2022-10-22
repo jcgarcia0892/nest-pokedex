9502ded10d8d2b2d8e0808c8ab443fa1664b8e7a
Julio Garcia <jcgarciam0892@gmail.com>
2022-10-22 08:37:57 -0300

    Fin seccion 7

---
+++ src/pokemon/pokemon.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { ParseMongoIdPipePipe } from 'src/common/pipes/parse-mongo-id-pipe.pipe';
+
+@Controller('pokemon')
+export class PokemonController {
+  constructor(private readonly pokemonService: PokemonService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.OK) // Esto sirve para modificar lo codigo de status 200, 300, 400, si no se coloca nest coloca uno por defecto
+  create(@Body() createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+
+    return this.pokemonService.create(createPokemonDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.pokemonService.findAll();
+  }
+
+  @Get(':term')
+  findOne(@Param('term') term: string) {
+    return this.pokemonService.findOne(term);
+  }
+
+  @Patch(':term')
+  update(
+    @Param('term') term: string,
+    @Body() updatePokemonDto: UpdatePokemonDto,
+  ) {
+    return this.pokemonService.update(term, updatePokemonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseMongoIdPipePipe) id: string) {
+    return this.pokemonService.remove(id);
+  }
+}
